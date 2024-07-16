8e7e6ba6ea062950272a74f4258a81c3cc34a7d3
LeonardoDimajo <joscha.meurer@hotmail.de>
2024-07-16 10:31:38 +0200

    04a private Fehlermeldung

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,12 +2,11 @@ package com.cc.java;
 
 public class Cat {
     
+    private String name;
+    private String  furColor;
+    private int age = 29;
     
-    // Felder / fields / attributes   
-    public String name;  // pass by reference
-    public String  furColor;
-    public int age = 29;   // pass by value
-    
+    // Konstrutor (keinen Rückgabetyp)
     public Cat(String name, String furColor, int age) {
         this.name = name;
         this.furColor = furColor;
@@ -15,6 +14,14 @@ public class Cat {
     }
 
 
+
+
+
+
+
+
+
+    
     // Rückgabetyp -- Cat
     public Cat getInstanceVariable(){
         return this;
